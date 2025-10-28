e900957ff1b90cadf0d0947e85ecf7c35a6053bf
Haida Houssam <hossamhaida4@gmail.com>
2025-10-28 10:25:33 +0100

    created empty project

---
+++ src/main/java/boss/team/conferencemicroservice/ConferenceMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package boss.team.conferencemicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceMicroServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConferenceMicroServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/boss/team/conferencemicroservice/ConferenceMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package boss.team.conferencemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceMicroServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
